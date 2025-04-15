b00dd881119be2ef141f7a7ffce4fee8ced46bab
Tanvir Ahmad <tanvir.info07@gmail.com>
2025-04-15 21:00:16 +0600

    test deployment

--- app/dashboard/lists/page.jsx
+++ app/dashboard/lists/page.jsx
@@ -1,7 +1,24 @@
+import { getAllApplications } from "@/utils/fetcher";
+import Link from "next/link";
 import React from "react";
 
-const CertificateListPage = () => {
-  return <div>hello</div>;
+const CertificateListPage = async () => {
+  const allApplications = await getAllApplications();
+  return (
+    <div>
+      {allApplications.length > 0 &&
+        allApplications.map((application) => (
+          <div
+            className="flex flex-col mt-5 items-center justify-center gap-2"
+            key={application._id}
+          >
+            <Link href={`/fitness_exam_wf/${application?.uniqueValue}`}>
+              {application.name}
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default CertificateListPage;
--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,7 +1,10 @@
 export const getAllApplications = async () => {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/application`
+      `${process.env.NEXT_PUBLIC_API_URL}/application`,
+      {
+        cache: "no-store",
+      }
     );
     const data = await response.json();
     return data;
@@ -9,10 +12,14 @@ export const getAllApplications = async () => {
     return { error: true, message: "data fetch error" };
   }
 };
+
 export const getSingleApplication = async (id) => {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/application/${id}`
+      `${process.env.NEXT_PUBLIC_API_URL}/application/${id}`,
+      {
+        cache: "no-store",
+      }
     );
     const data = await response.json();
     return data;
